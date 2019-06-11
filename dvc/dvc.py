9d9c7548667feb0606020c083f493592d6e707bb
Simone Iacono <simo.1299@@live.it>
2019-06-11 20:13:26 +0200

    Modified dvc script

--- dvc/dvc.py
+++ dvc/dvc.py
@@ -1,24 +1,27 @@
-import sys, getopt, getpass,mysql.connector
+import sys, os, getopt, getpass,mysql.connector
+
+def getvalue(message,default):
+    try:
+        result = input(message)
+    except SyntaxError:
+        result = default
+    return default
 
 def create(serviceName):
     if serviceName.casefold() == "mysql":
-        try:
-            hostname = input("Insert the hostname (default 127.0.0.1):")
-        except SyntaxError:
-            hostname = '127.0.0.1'
-        try:
-            port = input("Insert the port (default 3306):")
-        except SyntaxError:
-            port = '3306'
-        try:
-            username = input("Insert the username (default 'root'):")
-        except SyntaxError:
-            port = 'root'
+        hostname = getvalue("Insert the hostname (default 127.0.0.1):","127.0.0.1")
+        port = getvalue("Insert the port (default 3306):","3306")
+        username = input("Insert the username (default 'root'):","root")
         try:
             passwd = getpass.getpass("Insert the password (defaulf none):")
         except Exception as error:
             passwd = ''
-        dbname = input("Insert the db name")
+        try:
+            dbname = input("Insert the db name")
+        except Exception as error:
+            print("Database name not valid: "+error.with_traceback)
+            return
+
         query = """SELECT table_name FROM INFORMATION_SCHEMA.tables WHERE table_schema=`%s`"""
         connection = mysql.connector.connect(host=hostname, database = dbname,user=username,password = passwd,port = port)
         cursor = connection.cursor(prepared=True)
